75682a28b6d5a49e57bcc412c743cd85719c4da1
ulugbekdesign <otabekoff458@gmail.com>
2026-08-24 16:53:27 +0500

    Eski portfolio o'zgarishini qaytarish

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -8,10 +8,10 @@ const projects = [
   },
   {
     title: {
-      uz: "Branding",
-      ru: "Брендинг",
+      uz: "SMM Kampaniya",
+      ru: "SMM Кампания",
     },
-    image: "/logo.jpg",
+    image: "/socialmedia2.jpg",
   },
   {
     title: {
@@ -22,10 +22,45 @@ const projects = [
   },
   {
     title: {
+      uz: "Landing Sahifa",
+      ru: "Лендинг",
+    },
+    image: "/webdesign2.jpg",
+  },
+  {
+    title: {
+      uz: "Poster Dizayn",
+      ru: "Дизайн постера",
+    },
+    image: "/poster.jpg",
+  },
+  {
+    title: {
+      uz: "Kreativ Poster",
+      ru: "Креативный постер",
+    },
+    image: "/poster2.jpg",
+  },
+  {
+    title: {
+      uz: "Logo Dizayn",
+      ru: "Дизайн логотипа",
+    },
+    image: "/logo.jpg",
+  },
+  {
+    title: {
       uz: "Poligrafiya",
       ru: "Полиграфия",
     },
     image: "/poygrafya.jpg",
+  },
+  {
+    title: {
+      uz: "Bosma Materiallar",
+      ru: "Печатные материалы",
+    },
+    image: "/polygrafiya2.jpg",
   },
 ];
 
@@ -36,27 +71,29 @@ type PortfolioProps = {
 export default function Portfolio({ t }: PortfolioProps) {
   return (
     <section id="portfolio">
-      <h2 className="text-5xl font-bold mb-12">{t.portfolio.title}</h2>
+      <h2 className="text-5xl font-bold mb-12">
+  {t.portfolio.title}
+</h2>
 
-      <div className="grid md:grid-cols-2 gap-8">
-        {projects.map((project) => (
-          <div
-            key={project.image}
-            className="group overflow-hidden rounded-3xl bg-zinc-900"
-          >
-            <img
-              src={project.image}
-              alt={project.title[t.language as keyof typeof project.title]}
-              className="w-full h-72 object-cover transition duration-500 group-hover:scale-110"
-            />
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+       {projects.map((project) => (
+  <div
+    key={project.image}
+    className="group overflow-hidden rounded-3xl bg-zinc-900"
+  >
+    <img
+      src={project.image}
+     alt={project.title[t.language as keyof typeof project.title]}
+      className="w-full h-72 object-cover transition duration-500 group-hover:scale-110"
+    />
 
-            <div className="p-6">
-              <h3 className="text-xl font-semibold">
-                {project.title[t.language as "uz" | "ru"]}
-              </h3>
-            </div>
-          </div>
-        ))}
+    <div className="p-6">
+      <h3 className="text-xl font-semibold">
+        {project.title[t.language as "uz" | "ru"]}
+      </h3>
+    </div>
+  </div>
+))}
       </div>
     </section>
   );
